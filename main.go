c3815d462166ad46a55c22ff593979dffa8cab5e
= <=>
2024-05-18 12:42:21 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for number := 1; number < 100; number++ {
+		switch {
+		case number%3 == 0 && number%5 == 0:
+			fmt.Println("Pin Pan")
+		case number%3 == 0:
+			fmt.Println("Pin")
+		case number%5 == 0:
+			fmt.Println("Pan")
+		default:
+			fmt.Println(number)
+		}
+	}
+}
